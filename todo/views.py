d242217e7d44bbd1160d3c0f47918ba708e9240d
Arun Neelakandan <arun.neelakandan@gmail.com>
2011-02-13 21:51:34 +1100

    Added comments to the todo index view.

--- todo/views.py
+++ todo/views.py
@@ -3,11 +3,12 @@ from django.core.context_processors import csrf
 from django.template import RequestContext # For CSRF
 from django.forms.formsets import formset_factory, BaseFormSet
 from django.http import HttpResponse, HttpResponseRedirect
-from django.core.urlresolvers import reverse
 
 from dynamicform.todo.forms import *
 
 def index(request):
+    # This class is used to make empty formset forms required
+    # See http://stackoverflow.com/questions/2406537/django-formsets-make-first-required/4951032#4951032
     class RequiredFormSet(BaseFormSet):
         def __init__(self, *args, **kwargs):
             super(RequiredFormSet, self).__init__(*args, **kwargs)
@@ -18,6 +19,7 @@ def index(request):
 
     if request.method == 'POST': # If the form has been submitted...
         todo_list_form = TodoListForm(request.POST) # A form bound to the POST data
+        # Create a formset from the submitted data
         todo_item_formset = TodoItemFormSet(request.POST, request.FILES)
         
         if todo_list_form.is_valid() and todo_item_formset.is_valid():
@@ -32,6 +34,8 @@ def index(request):
         todo_list_form = TodoListForm()
         todo_item_formset = TodoItemFormSet()
     
+    # For CSRF protection
+    # See http://docs.djangoproject.com/en/dev/ref/contrib/csrf/ 
     c = {'todo_list_form': todo_list_form,
          'todo_item_formset': todo_item_formset,
         }
